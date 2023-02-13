d9f014204fac96cf80fdb30d3a08b6492611dc1d
martaprosniak <martaprosniak@gmail.com>
2023-02-13 22:24:04 +0100

    Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import Header from './components/header/Header'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      MAGIC 8 BALL
+      <Header/>
     </div>
   )
 }
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react';
+import './Header.css'
+
+const Header: FC = () => {
+  return <div className='logo'>Magic 8 Ball</div>;
+};
+
+export default Header;
