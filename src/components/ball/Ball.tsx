129206a0815ec67b226dd357cd570c77dd164444
martaprosniak <martaprosniak@gmail.com>
2023-02-14 00:24:01 +0100

    Random answer on click

--- src/components/ball/Ball.tsx
+++ src/components/ball/Ball.tsx
@@ -1,8 +1,18 @@
 import { FC } from "react";
 import './Ball.css'
+import data from './answers.json'
 
 const Ball: FC = () => {
-    return <div className="wrapper"><div className="ball"><span className="eight">8</span></div></div>
+
+    const handleClick = () => {
+        alert(data.answers[getRandomNumber(20)])
+    }
+
+    const getRandomNumber = (max: number) => {
+        return Math.floor(Math.random() * max)
+    }
+
+    return <div className="wrapper"><div className="ball" onClick={handleClick}><span className="eight">8</span></div></div>
 }
 
 export default Ball;
