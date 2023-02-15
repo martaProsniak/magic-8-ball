03aa611a5fce734bc05471cdaa55e445bce1d6e2
martaprosniak <martaprosniak@gmail.com>
2023-02-15 21:04:47 +0100

    Displaying answer

--- src/components/ball/Ball.tsx
+++ src/components/ball/Ball.tsx
@@ -1,18 +1,24 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import './Ball.css'
 import data from './answers.json'
 
 const Ball: FC = () => {
+    const [answer, setAnswer] = useState('')
 
     const handleClick = () => {
-        alert(data.answers[getRandomNumber(20)])
+        setAnswer(data.answers[getRandomNumber(20)])
     }
 
     const getRandomNumber = (max: number) => {
         return Math.floor(Math.random() * max)
     }
 
-    return <div className="wrapper"><div className="ball" onClick={handleClick}><span className="eight">8</span></div></div>
+    return (<div className="wrapper">
+        <div className="ball" onClick={handleClick}>
+            {!answer && <span className="eight">8</span>}
+            {answer && <span className="answer">{answer}</span>}
+        </div>
+    </div>)
 }
 
 export default Ball;
