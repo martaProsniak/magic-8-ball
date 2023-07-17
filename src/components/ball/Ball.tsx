3dce575080cdba1c06627631fb8c31177c47ffc3
martaprosniak <martaprosniak@gmail.com>
2023-07-17 22:55:16 +0200

    Adding favicon and footer

--- src/components/ball/Ball.tsx
+++ src/components/ball/Ball.tsx
@@ -1,9 +1,9 @@
-import { FC, useState } from "react";
+import { FC, useRef, useState } from "react";
 import './Ball.css'
 import data from './answers.json'
 
 const Ball: FC = () => {
-    const [answer, setAnswer] = useState('')
+    const [answer, setAnswer] = useState('');
 
     const handleClick = () => {
         setAnswer(data.answers[getRandomNumber(20)])
@@ -13,7 +13,7 @@ const Ball: FC = () => {
         return Math.floor(Math.random() * max)
     }
 
-    const instructionText = !answer ? 'Think of a question and tap the ball to get the answer' : 'Don\'t like the answer? Click the ball to ask again'
+    const instructionText = !answer ? 'Think of a question and tap the ball to get the answer' : 'Don\'t like the answer? Tap the ball to ask again'
 
     return (<div className="wrapper" >
         <div className="instruction">{instructionText}</div>
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 import './Footer.css'
 
 const Footer: FC = () => {
-    return <div className="footer">Footer</div>
+    return <div className="footer"><a href="https://github.com/martaProsniak" rel="noopener noreferrer" target="_blank">&copy; 2023 Marta Prośniak-Kalwaj</a></div>
 }
 
 export default Footer
