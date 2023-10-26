1e4557552856359cfc0cd5c08ec144ba5d4b11a7
martaprosniak <martaprosniak@gmail.com>
2023-10-26 22:32:06 +0200

    Fields added to manifest

--- vite.config.ts
+++ vite.config.ts
@@ -15,25 +15,67 @@ export default defineConfig({
         start_url: "https://8ballwilltellyou.netlify.app/",
         icons: [
           {
-            src: "/android-chrome-192x192.png",
+            src: "android/android-launchericon-512-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "android/android-launchericon-192-192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/android-chrome-512x512.png",
-            sizes: "512x512",
+            src: "android/android-launchericon-144-144.png",
+            sizes: "144x144",
+            type: "image/png",
+          },
+          {
+            src: "android/android-launchericon-96-96.png",
+            sizes: "96x96",
+            type: "image/png",
+          },
+          {
+            src: "android/android-launchericon-72-72.png",
+            sizes: "72x72",
+            type: "image/png",
+          },
+          {
+            src: "android/android-launchericon-48-48.png",
+            sizes: "48x48",
             type: "image/png",
           },
           {
             src: "/maskable_icon_x192.png",
             sizes: "196x196",
             type: "image/png",
-            purpose: "any maskable",
+            purpose: "maskable",
+          },
+          {
+            src: "/logo-512x512.png",
+            sizes: "196x196512x512",
+            type: "image/png",
+            purpose: "any",
           },
         ],
         theme_color: "#242424",
         background_color: "#242424",
         display: "standalone",
+        orientation: "portrait-primary",
+        display_override: ["window-controls-overlay", "minimal-ui", "browser"],
+        categories: ["entertainment", "lifestyle"],
+        description: "Have a question? Get the answer quick by adding this app to your home screen!",
+        screenshots: [
+          {
+            src: "screenshot1.png",
+            sizes: "586x1041",
+            label: "Magic 8 Ball, legendary fortune teller",
+          },
+          {
+            src: "screenshot2.png",
+            sizes: "586x1041",
+            label: "Sometimes you;ll have to ask again",
+          },
+        ],
       },
       workbox: {
         globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
