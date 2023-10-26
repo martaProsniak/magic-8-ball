8c3c7afdf662e3b31652ee7945b6e7a2d0c7d167
martaprosniak <martaprosniak@gmail.com>
2023-10-26 23:32:58 +0200

    Test beforeinstallprompt event

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,32 @@ import './App.css'
 import Header from './components/header/Header'
 import Ball from './components/ball/Ball'
 import Footer from './components/footer/Footer'
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [deferredPrompt, setDeferredPrompt] = useState<any>();
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+      .register('/sw.js')
+      .then(function () {
+        console.log('Service worker registered!');
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  }
+
+  useEffect(() => {
+    console.log(deferredPrompt);
+  }, [deferredPrompt])
+
+  window.addEventListener('beforeinstallprompt', function (event) {
+    console.log('beforeinstallprompt fired');
+    event.preventDefault();
+    setDeferredPrompt(event)
+    return false;
+  });
+
   return (
     <div className="App">
       <Header />
