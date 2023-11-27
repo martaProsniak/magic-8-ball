30ab3d4ef94671b41267a4103a33611e116ebb7a
mpk <martaprosniak@gmail.com>
2023-11-27 16:35:36 +0100

    Removing other logs

--- src/App.tsx
+++ src/App.tsx
@@ -32,16 +32,12 @@ function App() {
   if ("serviceWorker" in navigator) {
     navigator.serviceWorker
       .register("/sw.js")
-      .then(function () {
-        console.log("Service worker registered!");
-      })
       .catch(function (err) {
         console.log(err);
       });
   }
 
   window.addEventListener("beforeinstallprompt", function (event) {
-    console.log("beforeinstallprompt fired");
     event.preventDefault();
     setDeferredPrompt(event);
     return false;
