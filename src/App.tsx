2eb22df9a2ce850a2aa2c48444c99bc51582f1f9
mpk <martaprosniak@gmail.com>
2023-11-27 12:54:40 +0100

    Install Prompt animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,34 @@
-import './App.css'
-import Header from './components/header/Header'
-import Ball from './components/ball/Ball'
-import Footer from './components/footer/Footer'
-import { useState, useEffect } from 'react';
+import "./App.css";
+import Header from "./components/header/Header";
+import Ball from "./components/ball/Ball";
+import Footer from "./components/footer/Footer";
+import InstallPrompt from './components/installPrompt/InstallPrompt'
+import { useState, useEffect } from "react";
 
 function App() {
   const [deferredPrompt, setDeferredPrompt] = useState<any>();
-  const [clicks, setClicks] = useState<number>(() => Number(localStorage.getItem('clicks')) ?? 0);
+  const [clicks, setClicks] = useState<number>(
+    () => Number(localStorage.getItem("clicks")) ?? 0
+  );
+  const [promptDismissed, setPromptDismissed] = useState<boolean>(
+    () => !!localStorage.getItem("promptDismissed")
+  );
 
   const increaseClicks = () => {
     setClicks(clicks + 1);
-    localStorage.setItem('clicks', clicks.toString())
-  }
-  
-  if ('serviceWorker' in navigator) {
+    localStorage.setItem("clicks", clicks.toString());
+  };
+
+  const dismissPrompt = () => {
+    localStorage.setItem("promptDismissed", "true");
+    setPromptDismissed(false);
+  };
+
+  if ("serviceWorker" in navigator) {
     navigator.serviceWorker
-      .register('/sw.js')
+      .register("/sw.js")
       .then(function () {
-        console.log('Service worker registered!');
+        console.log("Service worker registered!");
       })
       .catch(function (err) {
         console.log(err);
@@ -26,22 +37,23 @@ function App() {
 
   useEffect(() => {
     console.log(deferredPrompt);
-  }, [deferredPrompt])
+  }, [deferredPrompt]);
 
-  window.addEventListener('beforeinstallprompt', function (event) {
-    console.log('beforeinstallprompt fired');
+  window.addEventListener("beforeinstallprompt", function (event) {
+    console.log("beforeinstallprompt fired");
     event.preventDefault();
-    setDeferredPrompt(event)
+    setDeferredPrompt(event);
     return false;
   });
 
   return (
     <div className="App">
+      <InstallPrompt deferredPrompt={deferredPrompt} dismissPrompt={dismissPrompt} />
       <Header />
       <Ball increaseClicks={increaseClicks} />
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/installPrompt/InstallPrompt.tsx
+++ src/components/installPrompt/InstallPrompt.tsx
@@ -1,3 +1,49 @@
+import { useRef } from "react";
+import "./InstallPrompt.css";
+
 interface InstallPromptProps {
-    deferredPrompt: any;
+  deferredPrompt: any;
+  dismissPrompt: () => void;
 }
+
+const InstallPrompt = ({
+  deferredPrompt,
+  dismissPrompt,
+}: InstallPromptProps) => {
+  const installPromptRef = useRef<HTMLDivElement>(null);
+  console.log(deferredPrompt);
+
+  if (deferredPrompt) {
+    installPromptRef.current?.classList.add("installPrompt--visible");
+  }
+
+  const hidePrompt = () => {
+    installPromptRef.current?.classList.remove("installPrompt--visible");
+    installPromptRef.current?.classList.add("installPrompt--hidden");
+  };
+
+  const handleInstall = async () => {
+    const userChoice = await deferredPrompt.prompt();
+    console.log(userChoice);
+    if (userChoice.outcome === "accepted") {
+          hidePrompt();
+    }
+  };
+
+  const handleNotNow = () => {
+    hidePrompt();
+  }
+
+  return deferredPrompt ? (
+    <div className="installPrompt" ref={installPromptRef}>
+      <p>Install App!</p>
+      <div>
+        <button onClick={handleInstall}>Install</button>
+        <button onClick={handleNotNow}>Not now</button>
+        <button>Dismiss</button>
+      </div>
+    </div>
+  ) : null;
+};
+
+export default InstallPrompt;
